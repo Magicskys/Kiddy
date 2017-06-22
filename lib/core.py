7af589da903e17d738c3a01c4aecaccfcd38e438
master <987083065@qq.com>
2017-06-23 00:04:16 +0800

    master

--- lib/core.py
+++ lib/core.py
@@ -3,7 +3,7 @@ import sqlite3
 from lib import logger
 import requests
 
-class Core():
+class Core(object):
     def __init__(self):
         self.conn=sqlite3.connect("database/core.db")
         self.conn.text_factory=str
@@ -15,30 +15,34 @@ class Core():
     def init(self):
         try:
             self.cu.execute("DROP TABLE info;")
+            self.cu.execute("DROP TABLE sniff")
             self.cu.execute("CREATE TABLE info (SESSION STRING,URL STRING,STATUS STRING,DATA STRING);")
+            self.cu.execute("CREATE TABLE sniff (SESSION STRING,METHOD STRING,URL STRING,COOKIE STRING,DATA STRING);")
             logger.success("初始化成功")
         except:
             logger.error("初始化错误")
             logger.info("从新建立数据库")
             self.cu.execute("CREATE TABLE info (SESSION STRING,URL STRING,STATUS STRING,DATA STRING);")
+            self.cu.execute("CREATE TABLE sniff (SESSION STRING,METHOD STRING,URL STRING,COOKIE STRING,DATA STRING);")
             logger.success("建立成功")
 
     def status(self,arg):
         if arg=="":
             sessions=self.cu.execute("SELECT SESSION FROM info;")
-            for session in sessions:
+            for session in self.cu.fetchall():
                 try:
-                    status=requests.get("127.0.0.1:8775/scan/" + session + "/status").json()['status']
+                    status=requests.get("http://127.0.0.1:8775/scan/" + session[3] + "/status").json()['status']
                     if status!="running":
-                        self.cu.execute("update info set status="+status['status']+"data="+status['data']+" where session='"+session+"';")
+                        self.cu.execute("update info set status="+status['status']+"data="+status['data']+" where session='"+session[0]+"';")
+                        self.conn.commit()
                     return self.cu.execute("SELECT SESSION,URL,STATUS FROM info;")
                 except:
                     logger.error("查询错误")
-                    logger.error("Sqlmap中没有这个%s任务"%session)
+                    logger.error("Sqlmap中没有这个%s任务"%session[0])
                     return self.cu.execute("SELECT SESSION,STATUS,URL FROM info;")
         else:
             try:
-                status = requests.get("127.0.0.1:8775/scan/" + arg + "/status").json()['status']
+                status = requests.get("http://127.0.0.1:8775/scan/" + arg[0] + "/status").json()['status']
                 if status != "running":
                     self.cu.execute("UPDATE info SET status=" + status['status'] + "data=" + status['data'] + " where session='" + arg + "';")
                 return self.cu.execute("SELECT SESSION,URL,STATUS FROM info WHERE SESSION='"+arg+"';")
@@ -55,7 +59,7 @@ class Core():
         except:
             logger.error("查询错误")
 
-    def save(self):
+    def save(self,arg):
         pass
 
     def count(self):
@@ -66,5 +70,14 @@ class Core():
         self.cu.execute("select count(*) from info where status='terminal'")
         return self.cu.fetchall()
 
-    def start(self):
-        pass
+    @classmethod
+    def create(cls,args):
+        self=Core()
+        # self.create_task(args)
+        sess=requests.get("http://127.0.0.1:8775/task/new").json()
+        # print args['DATA'],sess
+        self.cu.execute("INSERT INTO sniff values ('%s','%s','%s','%s','%s');" % (sess['taskid'], args['METHOD'], args['URL'], args['COOKIE'], str(args['DATA'].__dict__.items())[0]))
+        self.cu.execute("INSERT INTO info values ('%s','%s','%s','');" % (sess['taskid'], args['URL'], sess[u'success']))
+        self.conn.commit()
+        logger.info("扫描"+args['URL'])
+
